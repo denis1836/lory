11127596b6ee35b35e7e8fa062a060fcbd94cc91
denis1836 <den.pylypen@protonmail.com>
2026-09-17 17:39:09 +0200

    feat(db): add db pool connection

--- cmd/lory/main.go
+++ cmd/lory/main.go
@@ -1,16 +1,30 @@
 package main
 
 import (
+	"context"
 	"log"
 
 	"lory/internal/config"
+	"lory/internal/db"
 )
 
 func main() {
 	log.Println("Launching lory server...")
+	ctx := context.Background()
+
 	cfg, err := config.Load()
 	if err != nil {
 		log.Fatal("failed to load config %v", err)
 	}
+
+	Pool, err := db.NewPool(ctx, cfg.DBUrl)
+	if err != nil {
+		log.Fatal("failed to create db pool: %w", err)
+	}
+	defer func() {
+		log.Println("Closing db pool...")
+		Pool.Close()
+	}()
+	log.Println("Successfully connected to db!")
 
 }
--- internal/db/db.go
+++ internal/db/db.go
@@ -1,1 +1,29 @@
 package db
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func NewPool(ctx context.Context, dbURL string) (*pgxpool.Pool, error) {
+	c, err := pgxpool.ParseConfig(dbURL)
+	if err != nil {
+		return nil, fmt.Errorf("error parsing db URL: %w", err)
+	}
+
+	c.MaxConns = 10
+	c.MinConns = 2
+
+	pool, err := pgxpool.NewWithConfig(ctx, c)
+	if err != nil {
+		return nil, fmt.Errorf("unable to create a db pool: %w", err)
+	}
+
+	if err := pool.Ping(ctx); err != nil {
+		return nil, fmt.Errorf("unable to ping db: %w", err)
+	}
+
+	return pool, nil
+}
