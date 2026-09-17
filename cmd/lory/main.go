e3058ea9cabc073e49abbdc02b7a8d0a313bcaf1
denis1836 <den.pylypen@protonmail.com>
2026-09-17 07:47:51 +0200

    feat(config): add config loading and .env template

--- cmd/lory/main.go
+++ cmd/lory/main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"log"
+
+	"lory/internal/config"
+)
+
 func main() {
+	log.Println("Launching lory server...")
+	cfg, err := config.Load()
+	if err != nil {
+		log.Fatal("failed to load config %v", err)
+	}
 
 }
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,60 @@
 package config
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	Port      string
+	DBUrl     string
+	JWTSecret string
+	SMTPPass  string
+}
+
+func Load() (*Config, error) {
+	if err := godotenv.Load(); err != nil {
+		return nil, fmt.Errorf("failed to load .env file: %w", err)
+	}
+
+	// site
+	port := getEnvOrDefault("PORT", "5432")
+
+	// db
+	dbUser := os.Getenv("DB_USER")
+	dbPass := os.Getenv("DB_PASS")
+	dbHost := getEnvOrDefault("DB_HOST", "localhost")
+	dbPort := getEnvOrDefault("DB_PORT", "5432")
+	dbName := os.Getenv("DB_NAME")
+
+	if dbUser == "" || dbPass == "" || dbName == "" {
+		return nil, fmt.Errorf("missing .env fields (DB_USER, DB_PASS, DB_NAME)")
+	}
+
+	dbUrl := fmt.Sprintf("postgres://%s:%s/@%s:%s/%s?sslmode=disable", dbUser, dbPass, dbHost, dbPort, dbName)
+
+	// security
+	jwtSecret := os.Getenv("JWT_SECRET")
+	if jwtSecret == "" {
+		return nil, fmt.Errorf("failed to load JWT_SECRET")
+	}
+
+	// smtp
+	smtpPass := os.Getenv("SMTP_PASS")
+
+	return &Config{
+		Port:      port,
+		DBUrl:     dbUrl,
+		JWTSecret: jwtSecret,
+		SMTPPass:  smtpPass,
+	}, nil
+}
+
+func getEnvOrDefault(key, fallback string) string {
+	if val := os.Getenv(key); val != "" {
+		return val
+	}
+	return fallback
+}
