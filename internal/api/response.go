2ad9e05b81695e9d03b4567dee0e92351597c48f
denis1836 <den.pylypen@protonmail.com>
2026-09-18 17:44:15 +0200

    feat(api): add json parsing basic logic

---
+++ internal/api/response.go
@@ -0,0 +1,31 @@
+package api
+
+import (
+	"encoding/json"
+	"errors"
+	"io"
+	"net/http"
+)
+
+func JSON(w http.ResponseWriter, status int, data any) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	if data != nil {
+		_ = json.NewEncoder(w).Encode(data)
+	}
+}
+
+func Error(w http.ResponseWriter, status int, message string) {
+	JSON(w, status, map[string]string{"error": message})
+}
+
+func Decode[T any](r *http.Request) (T, error) {
+	var v T
+	if err := json.NewDecoder(r.Body).Decode(&v); err != nil {
+		if errors.Is(err, io.EOF) {
+			return v, errors.New("body cannot be empty")
+		}
+		return v, err
+	}
+	return v, nil
+}
